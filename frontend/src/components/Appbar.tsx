072b832ad1ea6dc6359262bbd55219c37f932fe6
yashginoya <yashginoya2003@gmail.com>
2024-03-08 01:26:38 +0530

    fixed avatar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
+import { useState } from "react";
 
 export const Appbar = () => {
+    const [name,setName] = useState(localStorage.getItem("username") || "Annonymous");
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={"/blogs"} className="flex justify-center flex-col cursor-pointer">
             Medium
@@ -10,7 +12,7 @@ export const Appbar = () => {
             <Link to={"/publish"}>
                 <button type="button" className=" mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New Blog</button>
             </Link>
-            <Avatar size={"big"} name="Yash" />
+            <Avatar size={"big"} name = {name} />
         </div>
     </div>
 }
